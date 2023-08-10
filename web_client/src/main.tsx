aca9084ccad8efddc6df372f143ffac698b2f933
Prasanjit Prakash <prasanjit@gmail.com>
2023-08-10 22:05:48 +0530

    Pinterest Layout working

--- web_client/src/main.tsx
+++ web_client/src/main.tsx
@@ -2,10 +2,27 @@ import "@rainbow-me/rainbowkit/styles.css";
 import { RainbowKitProvider } from "@rainbow-me/rainbowkit";
 import * as React from "react";
 import * as ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider, createRoutesFromElements, Route } from "react-router-dom"
 import { WagmiConfig } from "wagmi";
+import Root from "./layouts/root"
+import ErrorPage from "./pages/error"
 
-import { App } from "./App";
+import { App } from "./pages/App";
+import { Home } from "./pages/Home";
 import { chains, config } from "./wagmi";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<Root />}>
+      <Route index element={<Home />} />
+      <Route path="/users/:id" element={<Home />} />
+      <Route path="/users/:id/collection/" element={<Home />} />
+      <Route path="/collections/:id/" element={<Home />} />
+      <Route path="/collections" element={<Home />} />
+      <Route path="/app" element={<App />} />
+      <Route path="/error" element={<ErrorPage />} />
+    </Route>
+  ))
 
 /**
  * Root providers and initialization of app
@@ -17,7 +34,7 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <WagmiConfig config={config}>
       <RainbowKitProvider chains={chains}>
-        <App />
+        <RouterProvider router={router} />
       </RainbowKitProvider>
     </WagmiConfig>
   </React.StrictMode>,
